c3def09a6e4c784f973c158a4ab2c35033d95364
Jesse Tingle <jtingle0@gmail.com>
2021-06-30 16:23:23 +0000

    adds image for advert, subtotal section, and checkout title section

--- amazon/src/components/Checkout.js
+++ amazon/src/components/Checkout.js
@@ -4,7 +4,18 @@ import '../css/Checkout.css'
 function Checkout() {
     return (
         <div className="checkout">
-            <h1>This is the checkout page!</h1>
+            <div className="checkout__left">
+                <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" className="checkout__ad" />
+                <div>
+                    <h2 className="checkout__title">
+                        Shopping Cart
+                    </h2>
+                </div>
+            </div>
+
+            <div className="checkout__right">
+                <h2>The subtotal will go here.</h2>
+            </div>
         </div>
     )
 }
