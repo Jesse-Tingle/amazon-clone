5288d144eb09a18d52155e20f48c08ca693acc66
Jesse Tingle <jtingle0@gmail.com>
2021-06-30 16:01:54 +0000

    sets up basic outline for checkout page

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -2,21 +2,21 @@ import React from 'react'
 import "./App.css"
 import Header from "./components/Header"
 import Home from "./components/Home"
+import Checkout from './components/Checkout'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
 	return (
 		<Router>
 			<div className="app">
+				<Header />
 				<Switch>
 					<Route path='/'>
-						<Header />
 						<Home />
 					</Route>
 
 					<Route path='/checkout'>
-						<Header />
-						<h1>I'm the checkout!</h1>
+						<Checkout/>
 					</Route>
 				</Switch>
 			</div>
---
+++ amazon/src/components/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/Checkout.css'
+
+function Checkout() {
+    return (
+        <div className="checkout">
+            <h1>This is the checkout page!</h1>
+        </div>
+    )
+}
+
+export default Checkout
