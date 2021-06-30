183d867ecdb14dc59136dde5474077a60395d421
Jesse Tingle <jtingle0@gmail.com>
2021-06-30 15:41:30 +0000

    set's up browser router in App.js

--- amazon/src/App.js
+++ amazon/src/App.js
@@ -1,13 +1,26 @@
+import React from 'react'
 import "./App.css"
 import Header from "./components/Header"
 import Home from "./components/Home"
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
 	return (
-		<div className="app">
-			<Header />
-			<Home />
-		</div>
+		<Router>
+			<div className="app">
+				<Switch>
+					<Route path='/'>
+						<Header />
+						<Home />
+					</Route>
+
+					<Route path='/checkout'>
+						<Header />
+						<h1>I'm the checkout!</h1>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	)
 }
 
