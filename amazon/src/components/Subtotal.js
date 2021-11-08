2e693ece80ebf169f7b12672bd62c1453162c547
Jesse-Tingle <jtingle0@gmail.com>
2021-11-07 21:10:19 -0500

    finishes user auth and checkout pages

--- amazon/src/components/Subtotal.js
+++ amazon/src/components/Subtotal.js
@@ -1,30 +1,38 @@
-import React from 'react'
-import '../css/Subtotal.css'
-import CurrencyFormat from 'react-currency-format'
+import React from "react"
+import "../css/Subtotal.css"
+import CurrencyFormat from "react-currency-format"
+import { useStateValue } from "../context/StateProvider"
+import { getBasketTotal } from "../context/reducer"
+import { useHistory } from "react-router-dom"
 
 function Subtotal() {
-    return (
-        <div className='subtotal'>
-            <CurrencyFormat 
-                renderText={(value) => (
-                    <>
-                        <p>
-                            Subtotal (0 items): <strong>0</strong>
-                        </p>
-                        <small className='subtotal__gift'>
-                            <input type="checkbox" />This order contains a gift
-                        </small>
-                    </>
-                )}
-                decimalScale={2}
-                value={0}
-                displayType={'text'}
-                thousandSeparator={true}
-                prefix={'$'}
-            />
-            <button>Proceed to Checkout</button>
-        </div>
-    )
+	const history = useHistory()
+	const [{ basket }, dispatch] = useStateValue()
+
+	return (
+		<div className="subtotal">
+			<CurrencyFormat
+				renderText={(value) => (
+					<>
+						<p>
+							{/* Part of the homework */}
+							Subtotal ({basket.length} items): <strong>{value}</strong>
+						</p>
+						<small className="subtotal__gift">
+							<input type="checkbox" /> This order contains a gift
+						</small>
+					</>
+				)}
+				decimalScale={2}
+				value={getBasketTotal(basket)} // Part of the homework
+				displayType={"text"}
+				thousandSeparator={true}
+				prefix={"$"}
+			/>
+
+			<button>Proceed to Checkout</button>
+		</div>
+	)
 }
 
 export default Subtotal
--- amazon/src/context/reducer.js
+++ amazon/src/context/reducer.js
@@ -1,23 +1,55 @@
 export const initialState = {
-    cart: [],
-    user: null
-};
+	basket: [],
+	user: null,
+}
 
 // Selector
-export const getCartTotal = (cart) =>
-    cart?.reduce((amount, item) => item.price + amount, 0);
+export const getBasketTotal = (basket) =>
+	basket?.reduce((amount, item) => item.price + amount, 0)
 
 const reducer = (state, action) => {
-    console.log(action);
-    switch (action.type) {
-        case "ADD_TO_CART":
-            return {
-                ...state,
-                cart: [...state.cart, action.item],
-            };
-        default:
-            return state;
-    }
-};
+	console.log(action)
+	switch (action.type) {
+		case "ADD_TO_BASKET":
+			return {
+				...state,
+				basket: [...state.basket, action.item],
+			}
 
-export default reducer;
+		case "EMPTY_BASKET":
+			return {
+				...state,
+				basket: [],
+			}
+
+		case "REMOVE_FROM_BASKET":
+			const index = state.basket.findIndex(
+				(basketItem) => basketItem.id === action.id
+			)
+			let newBasket = [...state.basket]
+
+			if (index >= 0) {
+				newBasket.splice(index, 1)
+			} else {
+				console.warn(
+					`Cant remove product (id: ${action.id}) as its not in basket!`
+				)
+			}
+
+			return {
+				...state,
+				basket: newBasket,
+			}
+
+		case "SET_USER":
+			return {
+				...state,
+				user: action.user,
+			}
+
+		default:
+			return state
+	}
+}
+
+export default reducer
